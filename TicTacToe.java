8da637265a85351b72d2f9bb8c98c6689686ee6d
tehilaschechter <44352784+tehilaschechter@users.noreply.github.com>
2019-03-18 17:07:04 +0200

    Validate user input before passing to program

--- TicTacToe.java
+++ TicTacToe.java
@@ -19,10 +19,15 @@ public class TicTacToe {
 		
 		while(game) {
 			System.out.println("Player " + currPlayer.getCurrent() + ", it's your turn!");
+			
+			do {
 			System.out.print("Enter column: "); 
 			col = inputStream.nextInt();
 			System.out.print("Enter row: ");
 			row = inputStream.nextInt();
+			System.out.println("");
+			}
+			while(!validate(col, row, board)); 
 			
 			board.setValue(row, col, currPlayer);
 			board.printBoard();
@@ -32,6 +37,46 @@ public class TicTacToe {
 		
 		
 	}
+	
+	public static boolean validate(int colInput, int rowInput, GameBoard gb) {
+		
+		// If input entered is out of bounds
+		if(colInput < 1 || colInput > 3 || rowInput < 1 || rowInput > 3) {
+			System.out.println("Input entered is out of bounds. Please try again.");
+			return false;
+		}
+		
+		// Make sure space has not already been used
+		if(gb.getValue(rowInput, colInput) != '*') {
+			System.out.println("That spot is taken. Try again!");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static boolean ifWon(char curr, GameBoard gb) {
+		
+		// Across top row
+		
+		// Across middle row
+		
+		// Across bottom row
+		
+		// Down left column
+		
+		// Down middle column
+		
+		// Down right column
+		
+		// Diagonal left
+		
+		// Diagonal right
+			
+		
+		return false;		
+	}
+	
 	
 }
 
